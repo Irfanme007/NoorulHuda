e9b2c032279abc1a653b02d47606aebe7344672b
Irfan M E <139207046+Irfanme007@users.noreply.github.com>
2025-06-26 16:35:44 +0530

    Update settings.py

--- mosqueproject/settings.py
+++ mosqueproject/settings.py
@@ -159,7 +159,7 @@ PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'static/js', 'serviceworker.js'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_POST',cast=int)
+EMAIL_PORT = config('EMAIL_PORT',cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS',cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
